1085dba961778037276755829f716fdf5bcae6a4
David King <dking@ketralnis.com>
2015-05-26 11:14:50 -0700

    renames

--- test.sh
+++ test.sh
@@ -8,6 +8,6 @@ python -m exproxyment.multiproc \
    -- python -m exproxyment.simpleserver --logging=warn --port=7003 --version=present \
    -- python -m exproxyment.simpleserver --logging=warn --port=7004 --version=present \
    -- python -m exproxyment.simpleserver --logging=warn --port=7005 --version=past --insistent \
-   -- python -m exproxyment.exproxyment --logging=warn --port=7000 \
+   -- python -m exproxyment.server --logging=warn --port=7000 \
        --backends=localhost:7001,localhost:7002,localhost:7003,localhost:7004,localhost:7005 \
        "$@"
